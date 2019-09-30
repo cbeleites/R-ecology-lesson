c8bf430b32318143dbabfbb0a52d1f1f96dae3be
Katrin Leinweber <Katrin.Leinweber@tib.eu>
2019-09-30 17:56:15 +0200

    Regenerate figures & handout

--- code-handout.R
+++ code-handout.R
@@ -143,36 +143,8 @@
 
 
 
-## Indexing and subsetting data frames
+## Extracting vectors from data frames
 
-
-
-
-
-
-
-
-
-### Challenges:
-###
-### 1. Create a `data.frame` (`surveys_200`) containing only the
-###    data in row 200 of the `surveys` dataset.
-###
-### 2. Notice how `nrow()` gave you the number of rows in a `data.frame`?
-###
-###      * Use that number to pull out just that last row in the data frame
-###      * Compare that with what you see as the last row using `tail()` to make
-###        sure it's meeting expectations.
-###      * Pull out that last row using `nrow()` instead of the row number
-###      * Create a new data frame object (`surveys_last`) from that last row
-###
-### 3. Use `nrow()` to extract the row that is in the middle of the
-###    data frame. Store the content of this row in an object named
-###    `surveys_middle`.
-###
-### 4. Combine `nrow()` with the `-` notation above to reproduce the behavior of
-###    `head(surveys)`, keeping just the first through 6th rows of the surveys
-###    dataset.
 
 
 ### Factors
@@ -202,42 +174,6 @@ plot(surveys$sex)
 ## * Rename "F" and "M" to "female" and "male" respectively.
 ## * Now that we have renamed the factor level to "undetermined", can you recreate the
 ##   barplot such that "undetermined" is last (after "male")
-
-#> ## Compare the difference between our data read as `factor` vs `character`.
-#> surveys <- read.csv("data_raw/portal_data_joined.csv", stringsAsFactors = TRUE)
-#> str(surveys)
-#> surveys <- read.csv("data_raw/portal_data_joined.csv", stringsAsFactors = FALSE)
-#> str(surveys)
-#> ## Convert the column "plot_type" into a factor
-#> surveys$plot_type <- factor(surveys$plot_type)
-
-
-
-#> ## Challenge:
-#> ##  There are a few mistakes in this hand-crafted `data.frame`,
-#> ##  can you spot and fix them? Don't hesitate to experiment!
-#> animal_data <- data.frame(
-#>       animal = c(dog, cat, sea cucumber, sea urchin),
-#>       feel = c("furry", "squishy", "spiny"),
-#>       weight = c(45, 8 1.1, 0.8)
-#>       )
-
-
-
-#> ## Challenge:
-#> ##   Can you predict the class for each of the columns in the following
-#> ##   example?
-#> ##   Check your guesses using `str(country_climate)`:
-#> ##   * Are they what you expected? Why? why not?
-#> ##   * What would have been different if we had added `stringsAsFactors = FALSE`
-#> ##     when we created this data frame?
-#> ##   * What would you need to change to ensure that each column had the
-#> ##     accurate data type?
-#> country_climate <- data.frame(country = c("Canada", "Panama", "South Africa", "Australia"),
-#>                                climate = c("cold", "hot", "temperate", "hot/temperate"),
-#>                                temperature = c(10, 30, 18, "15"),
-#>                                northern_hemisphere = c(TRUE, TRUE, FALSE, "FALSE"),
-#>                                has_kangaroo = c(FALSE, FALSE, FALSE, 1))
 #> ## Pipes Challenge:
 #> ##  Using pipes, subset the data to include animals collected
 #> ##  before 1995, and retain the columns `year`, `sex`, and `weight.`
@@ -254,10 +190,10 @@ plot(surveys$sex)
 
 #> ## Mutate Challenge:
 #> ##  Create a new data frame from the `surveys` data that meets the following
-#> ##  criteria: contains only the `species_id` column and a column that
-#> ##  contains values that are half the `hindfoot_length` values (e.g. a
-#> ##  new column `hindfoot_half`). In this `hindfoot_half` column, there are
-#> ##  no NA values and all values are < 30.
+#> ##  criteria: contains only the `species_id` column and a new column called
+#> ##  `hindfoot_cm` containing the `hindfoot_length` values converted to centimeters.
+#> ##  In this `hindfoot_cm` column, there are no `NA`s and all values are less
+#> ##  than 3.
 #> 
 #> ##  Hint: think about how the commands should be ordered to produce this data frame!
 
@@ -291,14 +227,19 @@ plot(surveys$sex)
 
 
 
+
+
 #> ## Count Challenges:
-#> ##  1. How many animals were caught in each `plot_type` surveyed?
 #> 
-#> ##  2. Use `group_by()` and `summarize()` to find the mean, min, and max
+#> ##  1. Find out where the `n` is coming from, by reading the help pages of the above functions.
+#> 
+#> ##  2. How many animals were caught in each `plot_type` surveyed?
+#> 
+#> ##  3. Use `group_by()` and `summarize()` to find the mean, min, and max
 #> ## hindfoot length for each species (using `species_id`). Also add the number of
 #> ## observations (hint: see `?n`).
 #> 
-#> ##  3. What was the heaviest animal measured in each year? Return the
+#> ##  4. What was the heaviest animal measured in each year? Return the
 #> ##  columns `year`, `genus`, `species_id`, and `weight`.
 
 
@@ -321,14 +262,13 @@ plot(surveys$sex)
 
 #> ## Reshaping challenges
 #> 
-#> ## 1. Make a wide data frame with `year` as columns, `plot_id`` as rows, and where the values are the number of genera per plot. You will need to summarize before reshaping, and use the function `n_distinct` to get the number of unique genera within a chunk of data. It's a powerful function! See `?n_distinct` for more.
+#> ## 1. Spread the `surveys` data frame with `year` as columns, `plot_id` as rows, and the number of genera per plot as the values. You will need to summarize before reshaping, and use the function `n_distinct()` to get the number of unique genera within a particular chunk of data. It's a powerful function! See `?n_distinct` for more.
 #> 
-#> ## 2. Now take that data frame, and make it long again, so each row is a unique `plot_id` `year` combination
+#> ## 2. Now take that data frame and `gather()` it again, so each row is a unique `plot_id` by `year` combination.
 #> 
-#> ## 3. The `surveys` data set is not truly wide or long because there are two columns of measurement - `hindfoot_length` and `weight`.  This makes it difficult to do things like look at the relationship between mean values of each measurement per year in different plot types. Let's walk through a common solution for this type of problem. First, use `gather` to create a truly long dataset where we have a key column called `measurement` and a `value` column that takes on the value of either `hindfoot_length` or `weight`. Hint: You'll need to specify which columns are being gathered.
+#> ## 3. The `surveys` data set has two measurement columns: `hindfoot_length` and `weight`. This makes it difficult to do things like look at the relationship between mean values of each measurement per year in different plot types. Let's walk through a common solution for this type of problem. First, use `gather()` to create a dataset where we have a key column called `measurement` and a `value` column that takes on the value of either `hindfoot_length` or `weight`. *Hint*: You'll need to specify which columns are being gathered.
 #> 
-#> ## 4. With this new truly long data set, calculate the average of each `measurement` in each `year` for each different `plot_type`. Then `spread` them into a wide data set with a column for `hindfoot_length` and `weight`. Hint: Remember, you only need to specify the key and value columns for `spread`.
-#> 
+#> ## 4. With this new data set, calculate the average of each `measurement` in each `year` for each different `plot_type`. Then `spread()` them into a data set with a column for `hindfoot_length` and `weight`. *Hint*: You only need to specify the key and value columns for `spread()`.
 
 
 
@@ -458,95 +398,4 @@ plot(surveys$sex)
 #> ##  exercise or create a beautiful graph of your own. Use the RStudio
 #> ##  ggplot2 cheat sheet for inspiration:
 #> ##  https://www.rstudio.com/wp-content/uploads/2015/08/ggplot2-cheatsheet.pdf
-## SQL databases and R
 
-#> install.packages(c("dbplyr", "RSQLite"))
-
-
-
-library(dplyr)
-library(dbplyr)
-mammals <- DBI::dbConnect(RSQLite::SQLite(), "data_raw/portal_mammals.sqlite")
-
-
-
-tbl(mammals, sql("SELECT year, species_id, plot_id FROM surveys"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### Challenge
-## Write a query that returns the number of rodents observed in each
-## plot in each year.
-
-##  Hint: Connect to the species table and write a query that joins
-##  the species and survey tables together to exclude all
-##  non-rodents. The query should return counts of rodents by year.
-
-## Optional: Write a query in SQL that will produce the same
-## result. You can join multiple tables together using the following
-## syntax where foreign key refers to your unique id (e.g.,
-## `species_id`):
-
-## SELECT table.col, table.col
-## FROM table1 JOIN table2
-## ON table1.key = table2.key
-## JOIN table3 ON table2.key = table3.key
-
-
-
-
-### Challenge
-
-## Write a query that returns the total number of rodents in each
-## genus caught in the different plot types.
-
-##  Hint: Write a query that joins the species, plot, and survey
-##  tables together.  The query should return counts of genus by plot
-##  type.
-
-
-
-download.file("https://ndownloader.figshare.com/files/3299483",
-              "data_raw/species.csv")
-download.file("https://ndownloader.figshare.com/files/10717177",
-              "data_raw/surveys.csv")
-download.file("https://ndownloader.figshare.com/files/3299474",
-              "data_raw/plots.csv")
-library(tidyverse)
-species <- read_csv("data_raw/species.csv")
-surveys <- read_csv("data_raw/surveys.csv")
-plots <- read_csv("data_raw/plots.csv")
-
-my_db_file <- "data/portal-database-output.sqlite"
-my_db <- src_sqlite(my_db_file, create = TRUE)
-
-
-
-
-
-### Challenge
-
-## Add the remaining species table to the my_db database and run some
-## of your queries from earlier in the lesson to verify that you
-## have faithfully recreated the mammals database.
